ae7daf8c8f66d2612da887e2959ff3eeb8863d56
tg2021 <vldzlqn1230@naver.com>
2021-05-19 15:25:29 +0900

    2021-05-19 자바복습 자료형

---
+++ JAVA/ch02_DataType/DataTypeClassTheory.java
@@ -0,0 +1,38 @@
+package ch02_DataType;
+
+public class DataTypeClassTheory {
+    /*
+        컴퓨터는 데이터를 가지고 연산을 수행.
+        데이터의 연산의 결과로 자료가 만들어짐
+
+        데이터는 어떤 대상을 수치나 말로 표현한 것.
+        ex)
+        String book = "책";
+        int markNo = 7;
+    */
+
+    /*
+        1, 2, 3 등의 숫자를 정수라 함.
+        3.4, 4.5 등의 숫자를 실수라 함.
+        'A'는 문자, "회사"는 문자열이라고 분류함.
+
+        프로그래밍 언어에서도 여러 데이터 값의 종류를 지칭하는 데이터
+        타입이 존재함.
+        String, char, int, long, double, boolean 등의 데이터 타입이 존재.
+    */    
+
+    /*
+        char [문자] : '안', '녕', '하', '세', '요'.
+        String [문자열] : "안녕하세요."
+        byte[매우 작은 정수] : -128 ~ +127
+        short [작은 정수] : 약 -3만 ~ +3만
+        int [정수] : -21억 ~ +21억
+        long[큰 정수]: -900경 ~ +900경
+        float [실수], double [큰 실수]
+        bloolean [true, false]
+    */
+
+    /*
+        
+    */
+}

1a3f220cf806cc12bc337b7faf540a967c929b30
tg2021 <vldzlqn1230@naver.com>
2021-05-19 15:24:38 +0900

    2021-05-19 자바복습 주석

---
+++ JAVA/ch01_Comment/CommentClassTheory.java
@@ -0,0 +1,22 @@
+package ch01_Comment;
+/**
+    클래스주석
+ * 
+ */
+
+public class CommentClassTheory {
+    
+    public static void main(String[] args) {
+        /*
+         * 주석이란 프로그램에 대한 설명이나 어떤 절차 등을 설명하는 것
+         */
+
+        // 라인주석
+        /** API 블록 주석 */
+        /* 블록주석 */
+        System.out.println("Hello, world.");
+        //System.out.println("세계야, 안녕.");
+        
+    }
+
+}
